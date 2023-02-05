b59f741b571901657aa6811f2da77219f0444221
jasmin-8076 <111449105+jasmin-8076@users.noreply.github.com>
2023-02-05 22:10:55 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-14-SignUp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-14-demo-ymzfuh)
---
+++ src/app/models/userRegistration.ts
@@ -0,0 +1,24 @@
+import { FormControl } from "@angular/forms";
+
+export interface UserRegistration {
+  firstName: FormControl<string>;
+  lastName:FormControl<string>;
+  email: FormControl<string>;
+  mobNum: FormControl<number>;
+  password: FormControl<string>;
+  confirmPassword: FormControl<string>;
+  gender: FormControl<string>;
+  terms: FormControl<string>;
+  dateOfBirth:FormControl<string>;
+}
+
+export interface UserTypes {
+  firstName: string;
+  lastName: string;
+  email:string;
+  password:string;
+  gender:string;
+  mobNum: number;
+  dateofbirth:string;
+
+}
